61710a75a3beb7892a59b18e81747bdca8c925f9
Gabe <gdubs2000@gmail.com>
2023-04-26 10:47:25 -0600

    prep1-3

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        int grade = int.Parse(Console.ReadLine());
+        string letter = "";
+
+        if (grade >= 90)
+            letter = "A";
+        else if (grade >= 80)
+            letter = "B";
+        else if (grade >= 70)
+            letter = "C";
+        else if (grade >= 60)
+            letter = "D";
+        else
+            letter = "F";
+
+        int second_digit = grade % 10;
+        if (letter != "F" && grade < 97) {
+            if (second_digit >= 7)
+                letter+="+";
+            if (second_digit <= 3)
+                letter+="-";
+        }
+
+        Console.WriteLine($"You got a {letter}.");
+        if (grade >= 70) {
+            Console.WriteLine("Congratulations, you passed!");
+        } else {
+            Console.WriteLine("You didn't pass the class. Better luck next time!");
+        }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess = -1;
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,11);
+        int guesses = 0;
+
+        while (guess != number) {
+            Console.Write("What is the magic number? ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess > number) {
+                Console.WriteLine("Lower!");
+            }
+            if (guess < number) {
+                Console.WriteLine("Higher!");
+            }
+            guesses +=1; 
+        }
+        Console.WriteLine("You guessed it!");
+        Console.WriteLine($"It took you {guesses} tries.");
     }
 }
