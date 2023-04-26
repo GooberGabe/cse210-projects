7b801d08df1ddedde7f53487d8e3dad36e0e0366
Gabe <gdubs2000@gmail.com>
2023-04-26 12:25:18 -0600

    prep5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int input = -1;
+        List<int> numbers = new List<int>();
+
+        while (input != 0) {
+            Console.Write("Enter a number: ");
+            input = int.Parse(Console.ReadLine());
+            if (input != 0) numbers.Add(input);
+        }
+
+        int sum = 0;
+        int largest = 0;
+
+        foreach (int n in numbers) {
+            sum+=n;
+            if (n > largest) {
+                largest = n;
+            }
+        }
+
+        Console.WriteLine($"The sum is {sum}.");
+        Console.WriteLine($"The average is {sum / numbers.Count}.");
+        Console.WriteLine($"The largest number is {largest}.");
+
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -6,4 +6,28 @@ class Program
     {
         Console.WriteLine("Hello Prep5 World!");
     }
+
+    static void DisplayWelcome() {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName() {
+        Console.Write("What is your username?");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber() {
+        Console.Write("What is your favorite number?");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number) {
+        return (int)Math.Pow(number, 2);
+    }
+
+    static void DisplayResult() {
+        string name = PromptUserName();
+        int sqn = SquareNumber(PromptUserNumber());
+        Console.WriteLine($"{name}, the square of your number is {sqn}");
+    }
 }
