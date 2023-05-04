ba15eb56d997dc6b8f031e0e7bc40373b61ad6a5
Gabe <gdubs2000@gmail.com>
2023-05-04 17:30:51 -0600

    job

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Microsoft";
+        job2.Display();
+
+        Resume resume1 = new Resume();
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
+        
     }
 }
